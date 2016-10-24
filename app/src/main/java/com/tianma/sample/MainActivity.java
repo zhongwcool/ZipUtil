4bdda8997d91e069f678a6e235d4479dc652df50
tianma <1808931265@qq.com>
2016-10-24 22:11:11 +0800

    Accomplish the sample of zip file/directory.

---
+++ app/src/main/java/com/tianma/sample/MainActivity.java
@@ -0,0 +1,149 @@
+package com.tianma.sample;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.github.angads25.filepicker.controller.DialogSelectionListener;
+import com.github.angads25.filepicker.model.DialogConfigs;
+import com.github.angads25.filepicker.model.DialogProperties;
+import com.github.angads25.filepicker.view.FilePickerDialog;
+import com.tianma.zip.R;
+import com.tianma.zip.ZipUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Executor threadPool;
+
+    private String srcPath;
+    private String destPath;
+
+    private EditText srcEditText;
+    private EditText destEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+    private void init() {
+        findViewById(R.id.source_btn).setOnClickListener(mListener);
+        findViewById(R.id.dest_btn).setOnClickListener(mListener);
+        findViewById(R.id.zip_btn).setOnClickListener(mListener);
+        findViewById(R.id.unzip_btn).setOnClickListener(mListener);
+
+        srcEditText = (EditText) findViewById(R.id.source_edit_text);
+        destEditText = (EditText) findViewById(R.id.dest_edit_text);
+
+        threadPool = Executors.newSingleThreadExecutor();
+
+    }
+
+
+    private View.OnClickListener mListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.source_btn:
+                    chooseSource();
+                    break;
+                case R.id.dest_btn:
+                    chooseDestination();
+                    break;
+                case R.id.zip_btn:
+                    doZip();
+                    break;
+                case R.id.unzip_btn:
+                    doUnZip();
+                    break;
+            }
+        }
+    };
+
+    private void chooseSource() {
+        DialogProperties properties = new DialogProperties();
+        properties.selection_mode = DialogConfigs.SINGLE_MODE;
+        properties.selection_type = DialogConfigs.FILE_AND_DIR_SELECT;
+
+        FilePickerDialog dialog = new FilePickerDialog(this, properties);
+        dialog.show();
+        dialog.setDialogSelectionListener(new DialogSelectionListener() {
+            @Override
+            public void onSelectedFilePaths(String[] files) {
+                srcPath = files[0];
+                srcEditText.setText(srcPath);
+            }
+        });
+
+    }
+    private void chooseDestination() {
+        DialogProperties properties = new DialogProperties();
+        properties.selection_mode = DialogConfigs.SINGLE_MODE;
+        properties.selection_type = DialogConfigs.FILE_AND_DIR_SELECT;
+
+        FilePickerDialog dialog = new FilePickerDialog(this, properties);
+        dialog.show();
+        dialog.setDialogSelectionListener(new DialogSelectionListener() {
+            @Override
+            public void onSelectedFilePaths(String[] files) {
+                destPath = files[0];
+                destEditText.setText(destPath);
+            }
+        });
+    }
+
+    private void doZip() {
+        new ZipTask().executeOnExecutor(threadPool);
+    }
+
+    private void doUnZip() {
+        new UnzipTask().executeOnExecutor(threadPool);
+    }
+
+    private class ZipTask extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                ZipUtil.zip(new File(srcPath), new File(destPath));
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean bool) {
+            Toast.makeText(MainActivity.this, bool ? "Compress Success ^_^" : "Compress Failed T.T", Toast.LENGTH_SHORT).show();
+            super.onPostExecute(bool);
+        }
+    }
+
+    private class UnzipTask extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean bool) {
+            Toast.makeText(MainActivity.this, bool ? "Decompress Success ^_^" : "Decompress Failed T.T", Toast.LENGTH_SHORT).show();
+            super.onPostExecute(bool);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/tianma/zip/ZipUtil.java
@@ -0,0 +1,117 @@
+package com.tianma.zip;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * @author Tianma at 2016/10/24
+ */
+
+public class ZipUtil {
+
+
+    public static void unzip() {
+        new ZipDecompress().unzip();
+    }
+
+    /**
+     * 压缩文件或目录
+     *
+     * @param fileOrDir   被压缩的文件或目录
+     * @param destZipFile 目标压缩文件
+     * @throws IOException
+     */
+    public static void zip(File fileOrDir, File destZipFile) throws IOException {
+        new ZipCompress().zip(fileOrDir, destZipFile);
+    }
+
+    /**
+     * Zip解压缩
+     */
+    public static class ZipDecompress {
+
+        public void unzip() {
+        }
+
+    }
+
+    /**
+     * Zip压缩
+     */
+    public static class ZipCompress {
+
+        private static int buf = 4096;
+
+        /**
+         * 压缩文件或目录
+         *
+         * @param fileOrDir   被压缩的文件或目录
+         * @param destZipFile 目标压缩文件
+         * @throws IOException
+         */
+        public void zip(File fileOrDir, File destZipFile) throws IOException {
+            ZipOutputStream zos = null;
+            try {
+                zos = new ZipOutputStream(new FileOutputStream(destZipFile));
+                if (fileOrDir.isDirectory()) {
+                    zipDir(fileOrDir, fileOrDir, zos);
+                } else {
+                    zipFile(fileOrDir, fileOrDir.getParentFile(), zos);
+                }
+            } finally {
+                if (zos != null) {
+                    zos.close();
+                }
+            }
+        }
+
+        /**
+         * 压缩目录
+         *
+         * @param dir     被压缩的目录
+         * @param baseDir 被压缩的文件的根目录(用于记录压缩文件的层次结构)
+         * @param zos     压缩文件流
+         * @throws IOException
+         */
+        private void zipDir(File dir, File baseDir, ZipOutputStream zos) throws IOException {
+            File[] subFiles = dir.listFiles();
+            for (File subFile : subFiles) {
+                if (subFile.isDirectory()) {
+                    zipDir(subFile, baseDir, zos);
+                } else {
+                    zipFile(subFile, baseDir, zos);
+                }
+            }
+        }
+
+        /**
+         * 压缩文件
+         *
+         * @param srcFile 被压缩的文件
+         * @param baseDir 被压缩的文件的根目录(用于记录压缩文件层次结构)
+         * @param zos     压缩文件流
+         * @throws IOException
+         */
+        private void zipFile(File srcFile, File baseDir, ZipOutputStream zos) throws IOException {
+            InputStream in = new FileInputStream(srcFile);
+            String entry = srcFile.getPath().substring(baseDir.getPath().length() + 1);
+            ZipEntry zipEntry = new ZipEntry(entry);
+            zos.putNextEntry(zipEntry);
+            byte[] data = new byte[buf];
+            int len;
+            while ((len = in.read(data)) != -1) {
+                zos.write(data, 0, len);
+            }
+            zos.flush();
+            in.close();
+        }
+
+
+    }
+
+}
